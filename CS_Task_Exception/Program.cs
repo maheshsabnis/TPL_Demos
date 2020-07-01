5de8abdcac6eb5eedb422c58ef95e6bbdc15f2ae
maheshsabnis <sabnis_m@hotmail.com>
2020-07-01 23:09:11 +0530

    exception

---
+++ CS_Task_Exception/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CS_Task_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task1 = Task.Run(() => { throw new CustomException("This exception is expected!"); });
+
+            try
+            {
+                task1.Wait();
+            }
+            catch (AggregateException ae)
+            {
+                foreach (var e in ae.InnerExceptions)
+                {
+                    // Handle the custom exception.
+                    if (e is CustomException)
+                    {
+                        Console.WriteLine($"Exception Occured {e.Message}");
+                    }
+                    // Rethrow any other exception.
+                    else
+                    {
+                        throw;
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public class CustomException : Exception
+    {
+        public CustomException(String message) : base(message)
+        { }
+    }
+}
---
+++ CS_Task_Exception_App/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace CS_Task_Exception_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter x and y");
+                int x = Convert.ToInt32(Console.ReadLine());
+                int y = Convert.ToInt32(Console.ReadLine());
+
+                Task<int> t = Task<int>.Factory.StartNew(() => { return Divide(x, y); });
+
+
+                Console.WriteLine(t.Result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+
+            Console.ReadLine();
+        }
+
+        static int Divide(int x,int y)
+        {
+            if (y == 0)
+                throw new DivideByZeroException("Denominator cannot be zero {y}");
+
+            return x / y;
+
+        }
+    }
+}
