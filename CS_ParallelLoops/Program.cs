e294adab095be97679af2328d1f9023a777266f3
maheshsabnis <sabnis_m@hotmail.com>
2020-07-01 21:45:22 +0530

    Add project files.

---
+++ CS_ParallelLoops/Program.cs
@@ -0,0 +1,98 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CS_ParallelLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var _ = new EmployeeList();
+            Console.WriteLine($"Non-Parallel Processing Starts {DateTime.Now}");
+            var swNonParallel = Stopwatch.StartNew();
+            for (int i = 0; i < _.Count; i++)
+            {
+                ProcessTax.CalculateTax(_[i]);
+                Console.WriteLine($"Sequential Employee Record after tax {JsonSerializer.Serialize(_[i])}");
+            }
+            Console.WriteLine($"Non-Parallel Process completed at {DateTime.Now}" +
+                $"Total Time {swNonParallel.Elapsed.TotalSeconds}");
+
+
+            Console.WriteLine($"Parallel Processing Starts {DateTime.Now}");
+
+            var swParallel = Stopwatch.StartNew();
+            //Parallel.For(0, _.Count, i =>
+            //{
+            //    ProcessTax.CalculateTax(_[i]);
+            //    Console.WriteLine($"Parallel Employee Record after tax {JsonSerializer.Serialize(_[i])}");
+            //});
+            Parallel.ForEach<Employee>(_, emp =>
+            {
+                ProcessTax.CalculateTax(emp);
+                Console.WriteLine($"Parallel Employee Record after tax {JsonSerializer.Serialize(emp)}");
+            });
+            Console.WriteLine($"Parallel Process completed at {DateTime.Now}" +
+                $"Total Time {swParallel.Elapsed.TotalSeconds}");
+
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Employee
+    {
+        public int EmpNo { get; set; }
+        public string EmpName { get; set; }
+        public int Salary { get; set; }
+        public decimal Tax { get; set; }
+    }
+
+    public static class ProcessTax
+    {
+        public static Employee CalculateTax(Employee emp)
+        {
+            System.Threading.Thread.Sleep(100);
+            emp.Tax = emp.Salary * Convert.ToDecimal(0.4);
+            return emp;
+        }
+    }
+
+    public class EmployeeList : List<Employee>
+    {
+        public EmployeeList()
+        {
+            Add(new Employee() { EmpNo = 1, EmpName = "A", Salary = 21000 });
+            Add(new Employee() { EmpNo = 2, EmpName = "B", Salary = 22000 });
+            Add(new Employee() { EmpNo = 3, EmpName = "C", Salary = 23000 });
+            Add(new Employee() { EmpNo = 4, EmpName = "D", Salary = 24000 });
+            Add(new Employee() { EmpNo = 5, EmpName = "E", Salary = 25000 });
+            Add(new Employee() { EmpNo = 6, EmpName = "F", Salary = 26000 });
+            Add(new Employee() { EmpNo = 7, EmpName = "G", Salary = 27000 });
+            Add(new Employee() { EmpNo = 8, EmpName = "H", Salary = 28000 });
+            Add(new Employee() { EmpNo = 9, EmpName = "I", Salary = 29000 });
+            Add(new Employee() { EmpNo = 10, EmpName = "J", Salary = 30000 });
+            Add(new Employee() { EmpNo = 11, EmpName = "K", Salary = 31000 });
+            Add(new Employee() { EmpNo = 12, EmpName = "L", Salary = 32000 });
+            Add(new Employee() { EmpNo = 13, EmpName = "M", Salary = 33000 });
+            Add(new Employee() { EmpNo = 14, EmpName = "N", Salary = 34000 });
+            Add(new Employee() { EmpNo = 15, EmpName = "O", Salary = 35000 });
+            Add(new Employee() { EmpNo = 16, EmpName = "P", Salary = 36000 });
+            Add(new Employee() { EmpNo = 17, EmpName = "Q", Salary = 37000 });
+            Add(new Employee() { EmpNo = 18, EmpName = "R", Salary = 38000 });
+            Add(new Employee() { EmpNo = 19, EmpName = "S", Salary = 39000 });
+            Add(new Employee() { EmpNo = 20, EmpName = "T", Salary = 40000 });
+            Add(new Employee() { EmpNo = 21, EmpName = "U", Salary = 41000 });
+            Add(new Employee() { EmpNo = 22, EmpName = "V", Salary = 42000 });
+            Add(new Employee() { EmpNo = 23, EmpName = "W", Salary = 43000 });
+            Add(new Employee() { EmpNo = 24, EmpName = "X", Salary = 44000 });
+            Add(new Employee() { EmpNo = 25, EmpName = "Y", Salary = 45000 });
+            Add(new Employee() { EmpNo = 26, EmpName = "Z", Salary = 46000 });
+
+        }
+    }
+}
---
+++ CS_Tasks/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace CS_Tasks
+{
+    class Program
+    {
+      //  static Task gt1, gt2;
+
+        static void Main(string[] args)
+        {
+            RunTasks();
+            Console.WriteLine();
+            Console.WriteLine();
+            RunTaskRunMethod();
+            Console.WriteLine();
+            Console.WriteLine();
+            TaskContinueWith();
+            Console.ReadLine();
+        }
+
+
+        static void WriteConsoleOne(string msg)
+        {
+            Console.WriteLine($"The First Task {msg}");
+        }
+
+        static void WriteConsoleTwo(string msg)
+        {
+            Console.WriteLine($"The Second Task {msg}");
+        }
+        static void WriteConsoleThird(string msg)
+        {
+            Console.WriteLine($"The Third Task {msg}");
+        }
+
+        static void RunTasks()
+        {
+            var _ = Stopwatch.StartNew();
+            Console.WriteLine("Running Tasks....");
+            var task1 = Task.Factory.StartNew(()=> WriteConsoleOne("Message 1"));
+            var task2 = Task.Factory.StartNew(() => WriteConsoleTwo("Message 2"));
+            var task3 = Task.Factory.StartNew(() => WriteConsoleThird("Message 3"));
+            //task1.Wait();
+            //task2.Wait();
+            //task3.Wait();
+            Task.WaitAll(task1,task2,task3);
+            
+            _.Stop();
+            Console.WriteLine($"Tasks Stopped and completed in {_.Elapsed.TotalSeconds} time");
+        }
+
+        static void RunTaskRunMethod()
+        {
+            var _ = Stopwatch.StartNew();
+            Console.WriteLine("Running Tasks....");
+            var t1 = new Task(() => WriteConsoleOne("Message 1"));
+            t1.Start();
+            var t2 = new Task(() => WriteConsoleTwo("Message 2"));
+            t2.Start();
+            var t3 = new Task(() => WriteConsoleThird("Message 3"));
+            t3.Start();
+            _.Stop();
+            Console.WriteLine($"Tasks Stopped and completed in {_.Elapsed.TotalSeconds} time");
+        }
+
+        static void TaskContinueWith()
+        {
+            var _ = Stopwatch.StartNew();
+            Console.WriteLine("Running Tasks....");
+            Task task = Task.Factory.StartNew(() => WriteConsoleOne("Message 1"))
+                .ContinueWith(delegate { WriteConsoleTwo("Message 2"); })
+                .ContinueWith(delegate { WriteConsoleThird("Message 3"); });
+                 _.Stop();
+            Console.WriteLine($"Tasks Stopped and completed in {_.Elapsed.TotalSeconds} time");
+        }
+    }
+}
